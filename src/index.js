523b520047839fabe1ca7b230e48a00434a7e1d3
Raph <thesket5@outlook.fr>
2021-12-14 12:13:10 +0100

    Project casi fini, gestion des points, début de la gestion des défaites

--- src/index.js
+++ src/index.js
@@ -23,23 +23,61 @@ async function shuffleDeck() {
     return shuffle;
 }
 
+let value = {
+    "A": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "0": 10,
+    "J": 10,
+    "Q": 10,
+    "K": 10
+}
+
 const drawCrad = fetch("https://deckofcardsapi.com/api/deck/e3148dhynphx/draw/?count=2")
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
 
+        let score_finale = 0;
+
         let div = document.createElement("div");
+        div.style.display = "flex";
         document.body.appendChild(div);
 
+        let div_score1 = document.createElement("div");
+        div.append(div_score1);
+        let div_score2 = document.createElement("div");
+        div.append(div_score2);
+
         var myImage1 = new Image(150, 200);
-        console.log(data.cards[0].image);
         myImage1.src = data.cards[0].image;
-        div.append(myImage1);
+        div_score1.append(myImage1);
+        let score1 = data.cards[0].code.charAt(0);
+        let textScore1 = document.createElement("p")
+        textScore1.textContent += "Valeur 1 : " + value[score1];
+        div_score1.append(textScore1);
+        score_finale += value[score1];
 
         var myImage2 = new Image(150, 200);
-        console.log(data.cards[1].image);
         myImage2.src = data.cards[1].image;
-        div.append(myImage2);
+        div_score2.append(myImage2);
+        let score2 = data.cards[1].code.charAt(0);
+        let textScore2 = document.createElement("p")
+        textScore2.textContent += "Valeur 2 : " + value[score2];
+        div_score2.append(textScore2);
+        score_finale += value[score2];
+
+        let textScoreFinale = document.createElement("p")
+        textScoreFinale.id = "score_finale";
+        textScoreFinale.textContent += "Score : " + score_finale;
+        document.body.append(textScoreFinale);
 
         let btn = document.createElement("button");
         btn.innerHTML = "Relancer";
@@ -56,11 +94,28 @@ const drawCrad = fetch("https://deckofcardsapi.com/api/deck/e3148dhynphx/draw/?c
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
+
+                    let div_scoreX = document.createElement("div");
+                    div.append(div_scoreX);
+
                     var newImg = new Image(150, 200);
                     console.log(data.cards[0].image);
                     newImg.src = data.cards[0].image;
-                    div.append(newImg);
+                    div_scoreX.append(newImg);
+                    let scoreX = data.cards[0].code.charAt(0);
+                    let textScoreX = document.createElement("p")
+                    textScoreX.textContent += "Valeur de la carte : " + value[scoreX];
+                    div_scoreX.append(textScoreX);
+                    score_finale += value[scoreX];
+                    document.getElementById('score_finale').innerText = "Score : " + score_finale;
                 })
         }
         document.body.appendChild(btn2);
+
+        if (score_finale > 21) {
+            console.log(score_finale);
+            let loose = document.createElement("h1");
+            loose.innerText = "You loose";
+            div.append(loose);
+        }
     });
