8118e8d1f61f69aa5a64345e50d5858f334fce28
Raph <thesket5@outlook.fr>
2021-12-16 14:01:33 +0100

    New changement in index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ fetchDeckJSON().then(deck => {
     console.log(idDeck);
 });*/
 
-const url_shuffle = "https://deckofcardsapi.com/api/deck/e3148dhynphx/return/"
+const url_shuffle = "https://deckofcardsapi.com/api/deck/ref9gg88uwjm/return/"
 
 async function shuffleDeck() {
     const response = await fetch(url_shuffle);
@@ -41,38 +41,46 @@ let value = {
 }
 
 function drawNewCard() {
-    fetch("https://deckofcardsapi.com/api/deck/e3148dhynphx/draw/?count=1")
+    fetch("https://deckofcardsapi.com/api/deck/ref9gg88uwjm/draw/?count=1")
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
 
-            let div = document.getElementById('principale');
+            let div = document.getElementById('add_card');
             let div_scoreX = document.createElement("div");
             div.append(div_scoreX);
 
+            let newScore = document.getElementById('score').innerHTML;
+            newScore = Number(newScore.slice(-2));
+            let textScoreV = document.getElementById("newValue");
+
             var newImg = new Image(150, 200);
-            console.log(data.cards[0].image);
             newImg.src = data.cards[0].image;
+            newImg.style.marginLeft = "-100%";
+            newImg.style.transform = "rotate(-10deg)";
             div_scoreX.append(newImg);
+
             let scoreX = data.cards[0].code.charAt(0);
             let textScoreX = document.createElement("p")
-            textScoreX.textContent += "Valeur de la carte : " + value[scoreX];
+            textScoreV.innerText += "\n\nValeur de la nouvelle carte : " + data.cards[0].code + " - " + value[scoreX];
             div_scoreX.append(textScoreX);
-            score_finale += value[scoreX];
-            document.getElementById('score_finale').innerText = "Score : " + score_finale;
+            newScore += value[scoreX];
 
-            document.getElementById('nb_carte').innerText = "Nombre de cartes restantes : " + data.remaining
-            if (score_finale > 21) {
-                let loose = document.createElement("h1");
-                loose.innerText = "You loose, sale merde";
-                div.append(loose);
+            document.getElementById('score').innerText = "Score : " + newScore;
 
-                btn2.style.display = "none";
+            document.getElementById('carte_rest').innerText = "Nombre de cartes restantes : " + data.remaining
+
+            if (newScore > 21) {
+                let loose = document.getElementById('score').innerText = "You loose, sale merde, cliquer sur le deck pour relancer un jeu";
+
+                if (document.getElementById('deck').click) {
+                    window.location.reload();
+                }
             }
         })
 }
 
-const drawCrad = fetch("https://deckofcardsapi.com/api/deck/e3148dhynphx/draw/?count=2")
+const drawCrad = fetch("https://deckofcardsapi.com/api/deck/ref9gg88uwjm/draw/?count=2")
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
@@ -81,47 +89,31 @@ const drawCrad = fetch("https://deckofcardsapi.com/api/deck/e3148dhynphx/draw/?c
 
         let div = document.createElement("div");
         div.style.display = "flex";
+        div.style.margin = "5%";
         div.id = "principale";
         document.body.appendChild(div);
 
-        let div_score1 = document.createElement("div");
-        div.append(div_score1);
-        let div_score2 = document.createElement("div");
-        div.append(div_score2);
+        let div_score1 = document.getElementById('carte1');
+        let div_score2 = document.getElementById('carte2');
 
         var myImage1 = new Image(150, 200);
         myImage1.src = data.cards[0].image;
+        myImage1.style.transform = "rotate(-10deg)";
         div_score1.append(myImage1);
         let score1 = data.cards[0].code.charAt(0);
-        let textScore1 = document.createElement("p")
-        textScore1.textContent += "Valeur 1 : " + value[score1];
-        div_score1.append(textScore1);
+        document.getElementById('value1').innerText = "Valeur carte 1 : " + data.cards[0].code + " - " + value[score1]
         score_finale += value[score1];
 
         var myImage2 = new Image(150, 200);
         myImage2.src = data.cards[1].image;
+        myImage2.style.marginLeft = "-60%";
+        myImage2.style.transform = "rotate(25deg)";
         div_score2.append(myImage2);
         let score2 = data.cards[1].code.charAt(0);
-        let textScore2 = document.createElement("p")
-        textScore2.textContent += "Valeur 2 : " + value[score2];
-        div_score2.append(textScore2);
+        document.getElementById('value2').innerText = "Valeur carte 2 : " + data.cards[1].code + " - " + value[score2]
         score_finale += value[score2];
 
-        let textScoreFinale = document.createElement("p")
-        textScoreFinale.id = "score_finale";
-        textScoreFinale.textContent += "Score : " + score_finale;
-        document.body.append(textScoreFinale);
+        document.getElementById('score').innerText = "Votre score est de : " + score_finale;
 
-        let textNbCarte = document.createElement("p")
-        textNbCarte.id = "nb_carte";
-        textNbCarte.textContent += "Nombre de cartes restantes : " + data.remaining;
-        document.body.append(textNbCarte);
-
-        let btn = document.createElement("button");
-        btn.innerHTML = "Relancer";
-        btn.type = "submit";
-        btn.onclick = function() {
-            shuffleDeck();
-        }
-        document.body.appendChild(btn);
+        document.getElementById('carte_rest').innerText = "Nombre de cart restantes : " + data.remaining;
     });
